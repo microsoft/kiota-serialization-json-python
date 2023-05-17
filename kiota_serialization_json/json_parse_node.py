07ac9de802ca034e7bf67c1f0e65254a732c4884
balosh-daniel <60570645+balosh-daniel@users.noreply.github.com>
2023-05-17 11:27:38 +0300

    handle str in object_dict

--- kiota_serialization_json/json_parse_node.py
+++ kiota_serialization_json/json_parse_node.py
@@ -278,6 +278,9 @@ class JsonParseNode(ParseNode, Generic[T, U]):
         # if object is null
         if not object_dict:
             return
+        
+        if isinstance(object_dict, str):
+            object_dict = json.loads(object_dict)
 
         if isinstance(item, AdditionalDataHolder):
             item_additional_data = item.additional_data
