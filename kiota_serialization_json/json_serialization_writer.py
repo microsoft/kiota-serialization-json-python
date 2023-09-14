0d28a960b1df51c057c5847fa6c438af7beda153
samwelkanda <samwelkanda@gmail.com>
2023-09-14 21:41:22 +0300

    Fix linter issue

--- kiota_serialization_json/json_serialization_writer.py
+++ kiota_serialization_json/json_serialization_writer.py
@@ -411,7 +411,7 @@ class JsonSerializationWriter(SerializationWriter):
         value.serialize(temp_writer)
         if isinstance(value, BackedModel):
             changed_keys = [k for k, v in value.backing_store.enumerate_()]
-            serialized_keys = [k for k in temp_writer.writer]
+            serialized_keys = list(temp_writer.writer)
             for key in list(set(serialized_keys) - set(changed_keys)):
                 # Discard unchanged values from serialization
                 del temp_writer.writer[key]
